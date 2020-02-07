1fe4689800cc3d02518c27f7d8150bf020c2239a
Ron Martin <ronmartin89@gmail.com>
2020-02-06 17:09:48 -0800

    Created form for AddFolder

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -6,4 +6,42 @@ import './AddFolder.css';
 
 export default class AddFolder extends React.Component {
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        };
+    }
+
+    // render the page. 
+    // Get user input from page
+    // use that input to make a POST fetch request
+    // do error handling
+    render() {
+        return (
+            <form className="AddFolder">
+            <h2>Add a New Folder</h2>
+            <div className="AddFolder__hint">* required field</div>  
+            <div className="form-group">
+              <label htmlFor="name">Folder name *</label>
+              <input 
+                type="text" 
+                className="AddFolder__control"
+                name="name" 
+                id="name"/>
+            </div>
+
+     
+            <div className="AddFolder__button__group">
+                <button type="reset" className="AddFolder__button">
+                    Cancel
+                </button>
+                <button type="submit" className="AddFolder__button">
+                    Save
+                </button>
+            </div>
+          </form>
+        );
+    }
 }
