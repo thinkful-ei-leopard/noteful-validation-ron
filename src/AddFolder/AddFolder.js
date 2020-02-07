50e339438fb4f55260c34d811fa73aaa81f0bb3a
Ron Martin <ronmartin89@gmail.com>
2020-02-06 18:18:59 -0800

    Doing validation

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -18,15 +18,30 @@ export default class AddFolder extends React.Component {
 
     updateName(name) {
         // event.target.value of the input
+        // Whenever the user types anything into the input, we update the state! each letter at a time!
         this.setState({name: {value: name}});
         console.log(name);
       }
      
+    validateName() {
+        // this removes any whitespace from value and makes sure the value isn't an empty string
+        const name = this.state.name.value.trim(); 
+        if(name.length === 0) {
+            return 'Please enter a folder name';
+        } else if(name.length > 30) {
+            return 'Please keep the folder name under 30 characters; brevity is the soul of wit.'
+        }
+
+    }
+
     handleSubmit(event){
         event.preventDefault();
         // const { name, password, repeatPassword } = this.state;
         const name = this.state;
         console.log('Name: ', name);
+
+        // potentially submit these values to the server here
+
     }
 
     // render the page. 
