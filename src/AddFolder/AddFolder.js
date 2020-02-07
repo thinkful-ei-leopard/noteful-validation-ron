76f979d0c8ef35b0148395a6686c93a83f88e81a
Ron Martin <ronmartin89@gmail.com>
2020-02-06 17:54:42 -0800

    using controlled components now

--- src/AddFolder/AddFolder.js
+++ src/AddFolder/AddFolder.js
@@ -8,15 +8,28 @@ export default class AddFolder extends React.Component {
 
     constructor(props) {
         super(props)
-
+        // using React.createRef is another way to reference get the value from an event
+        // instead of using event.target.name.value. You then put ref={this.nameInput}
+        // One advantage is you dont need to know the "name" of the element since you set it to "this.nameInput"
+        // and reference it with this.nameInput.current.value instead of event.target.NAME.value
+        this.nameInput = React.createRef();
         this.state = {
-
+            name: {
+                value: ''
+              }
         };
     }
 
+    updateName(name) {
+        this.setState({name: {value: name}});
+      }
+     
     handleSubmit(event){
         event.preventDefault();
-        const name = event.target.name.value;
+        //const name = event.target.name.value;
+        // const name = this.nameInput.current.value;
+        // const { name, password, repeatPassword } = this.state;
+        const name = this.state;
         console.log('Name: ', name);
     }
 
@@ -30,12 +43,14 @@ export default class AddFolder extends React.Component {
                 <h2>Add a New Folder</h2>
                 <div className="AddFolder__hint">* required field</div>  
                 <div className="form-group">
-                <label htmlFor="name">Folder name *</label>
+                <label htmlFor="add-folder-name">Folder name *</label>
                 <input 
                     type="text" 
                     className="AddFolder__control"
-                    name="name" 
-                    id="name"/>
+                    name="add-folder-name" 
+                    id="add-folder-name"
+                    onChange={e => this.updateName(e)}
+                    required/>
                 </div>
 
         
