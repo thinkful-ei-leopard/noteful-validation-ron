d2476b795812592a1ae211914b25416bca924bcc
Ron Martin <ronmartin89@gmail.com>
2020-02-06 16:49:27 -0800

    Make baseline version of AddFolder and AddNote (empty stubs

---
+++ src/AddFolder/AddFolder.js
@@ -0,0 +1,9 @@
+// this.context.addFolder <-- event handler in App
+import React from 'react';
+import ApiContext from '../ApiContext';
+import config from '../config';
+import './AddFolder.css';
+
+export default class AddFolder extends React.Component {
+
+}
---
+++ src/AddNote/AddNote.js
@@ -0,0 +1,9 @@
+// this.context.addNote <-- event handler in App
+import React from 'react';
+import ApiContext from '../ApiContext';
+import config from '../config';
+import './AddNote.css';
+
+export default class AddNote extends React.Component {
+
+}
